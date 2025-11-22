38e4ce82c0e822d6e10031b4500dd05b15b8be7e
gauravashisth <miya.musashi.moto1645@gmail.com>
2025-11-22 17:39:49 +0530

    chore: change default font and add cursor pointer

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/components/ui/button";
 export default function Page() {
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <Button>button</Button>
+      <Button className="font-mono rounded-none">Click me</Button>
+      my portfolio
     </div>
   );
 }
