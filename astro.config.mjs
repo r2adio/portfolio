0df970bd6bba6aa4ec9c3590f94a8c0015a19e6f
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-03-03 16:18:21 +0530

    feat: add social icons and borders to footer component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
-
+import icon from "astro-icon";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  integrations: [react()],
+  vite: { plugins: [tailwindcss()] },
+  integrations: [react(), icon()],
 });
+
