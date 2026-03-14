5f30b8aa83be947129624eb612f9771326db046d
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-03-14 23:13:06 +0530

    chore: update deps and command-menu header icon

--- src/components/CommandMenu.tsx
+++ src/components/CommandMenu.tsx
@@ -10,7 +10,7 @@ import {
   CommandItem,
   CommandList,
 } from "@components/ui/command";
-import { SearchIcon } from "lucide-react";
+import { CommandIcon, SearchIcon } from "lucide-react";
 
 export function CommandMenu() {
   const [open, setOpen] = React.useState(false);
@@ -31,12 +31,15 @@ export function CommandMenu() {
     <>
       <button
         onClick={() => setOpen(true)}
-        className="flex items-center gap-1.5 text-sm"
+        className="group relative border rounded-2xl p-px"
       >
-        <SearchIcon className="h-4 w-4" />
-        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">⌘</span>K
-        </kbd>
+        <span className="flex items-center gap-1.5 rounded-2xl bg-background px-2 py-1 text-sm">
+          <SearchIcon className="h-4 w-4 text-muted-foreground stroke-3" />
+          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+            {/* <span className="text-xs">⌘</span>K */}
+            <CommandIcon className="h-4 w-4" />K
+          </kbd>
+        </span>
       </button>
       <CommandDialog open={open} onOpenChange={setOpen}>
         <Command>
